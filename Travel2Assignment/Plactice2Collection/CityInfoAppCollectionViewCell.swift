3fcccdd2dac278e8108e4508166e06c6d588ec15
judge_happily <you3192001@gmail.com>
2024-01-09 20:33:11 +0900

    과제 1,2번 까지 해결

---
+++ Travel2Assignment/Plactice2Collection/CityInfoAppCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CityInfoAppCollectionViewCell.swift
+//  Travel2Assignment
+//
+//  Created by Jae hyung Kim on 1/9/24.
+//
+
+import UIKit
+
+class CityInfoAppCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Travel2Assignment/PlacticeCollection/TravelCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TravelCollectionViewCell.swift
+//  Travel2Assignment
+//
+//  Created by Jae hyung Kim on 1/9/24.
+//
+
+import UIKit
+
+class TravelCollectionViewCell: UICollectionViewCell {
+    
+}
