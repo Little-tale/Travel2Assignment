c5c366ef6ad4dd2ef2cbc644019a42017a6b9bfd
judge_happily <you3192001@gmail.com>
2024-01-10 20:16:47 +0900

    내가 잘 구조체 썻는지도 이넘을 잘 썻는지도 모르겠는 정말 난 엉망이야

---
+++ Travel2Assignment/Plactice2Collection/Plactice_Struct+Enum.swift
@@ -0,0 +1,8 @@
+//
+//  Plactice_Struct+Enum.swift
+//  Travel2Assignment
+//
+//  Created by Jae hyung Kim on 1/10/24.
+//
+
+import Foundation
---
+++ Travel2Assignment/PlacticeCollection/PlacticeStruct.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+
+struct CellSizeForCity {
+    let screenWidth: CGFloat
+    let virticalSpacing: CGFloat = 16
+    let horSpacing: CGFloat = 16
+    
+    let height: CGFloat = 260
+    let top: CGFloat = 8
+    let bottom: CGFloat = 8
+    
+    let right: CGFloat = 24
+    let left: CGFloat = 24
+    // content + 1
+    let contentCount: CGFloat = 2
+}
